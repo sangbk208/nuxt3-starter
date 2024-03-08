9ace3864be7d82ef801294e4c14b481f6e500037
Sang Nguyen <sang.nguyen@cubicstack.net>
2024-03-08 16:16:53 +0700

    implement LanguageSwitcher CPN

---
+++ components/LanguageSwitcher/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" name="LanguageSwitcher" setup>
+const { locales, setLocale } = useI18n();
+
+const onChangeLanguage = (code: string) => {
+  setLocale(code);
+};
+
+const classes = {
+  container: 'flex items-center gap-2',
+  flagImg: 'size-4'
+};
+</script>
+
+<template>
+  <div :class="classes.container">
+    <button v-for="{ code, flag } in locales" :key="code" @click="() => onChangeLanguage(code)">
+      <img :src="`/assets/flags/${flag}.svg`" :alt="flag" :class="classes.flagImg" />
+    </button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,14 @@
 <script setup>
-const { locales, setLocale } = useI18n();
-
-const onChangeLanguage = (code) => {
-  setLocale(code);
-};
-
 const classes = {
   container: 'p-9 space-y-4',
-  flagWrapper: 'flex items-center gap-2',
-  flagImg: 'size-4'
+  title: 'text-lg font-bold'
 };
 </script>
 
 <template>
   <div :class="classes.container">
-    <div :class="classes.flagWrapper">
-      <button v-for="{ code, flag } in locales" :key="code" @click="() => onChangeLanguage(code)">
-        <img :src="`/assets/flags/${flag}.svg`" :alt="flag" :class="classes.flagImg" />
-      </button>
-    </div>
+    <LanguageSwitcher />
 
-    <h1 class="text-h1">{{ $t('title') }}</h1>
+    <h1 :class="classes.title">{{ $t('title') }}</h1>
   </div>
 </template>
